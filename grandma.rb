f54a70f5acbdf36b8dc2fa6a38704379933f4851
Emma Gouti <goutiemma@gmail.com>
2018-09-30 07:44:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(arg)
+  if arg != arg.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+    
+  else if arg == arg.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "I LOVE YOU TOO PUMPKIN!"
+ end
+  end 
+  
+  
