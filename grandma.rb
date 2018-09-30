290e5bca9933d120bca027204ba11d8c35da7c1d
Emma Gouti <goutiemma@gmail.com>
2018-09-30 07:46:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(arg)
   if arg != arg.upcase
     return "HUH?! SPEAK UP, SONNY!"
     
-  else if arg == arg.upcase
+  elsif arg == arg.upcase
     return "NO, NOT SINCE 1938!"
   else 
     return "I LOVE YOU TOO PUMPKIN!"
